e60b00922927e0c3c0490e1ceac0dbf253c624c6
BrenoMaia98 <brenomaia98@gmail.com>
2020-11-13 13:21:55 -0300

    added color pallet

---
+++ src/Constants/ColorPalette.ts
@@ -0,0 +1,9 @@
+export const ColorPalette = {
+  edit: '#1AAE9F',
+  danger: '#D3455B',
+  primaryText: '#252525',
+  secondaryText: '#8C8C8C',
+  tertiaryText: '#CFCFCF',
+  backgroundColorText: '#EFEFEF',
+  white: '#fff',
+};
