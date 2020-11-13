a6c6914e4bb8baa753e6367a756c7a1267411098
BrenoMaia98 <brenomaia98@gmail.com>
2020-11-13 15:18:38 -0300

    adding Palette styles and typhograpy

--- App.tsx
+++ App.tsx
@@ -1,9 +1,36 @@
+/* eslint-disable global-require */
 import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
 import StackNavigation from './routes/StackNavigation';
 
-export default function App() {
+export default function App(): JSX.Element {
+  const [fontloaded, setfontloaded] = React.useState(false);
+
+  const fetchFonts = async () => {
+    return Font.loadAsync({
+      'Roboto-Regular': require('./src/assets/fonts/Roboto/Roboto-Regular.ttf'),
+      'Roboto-Medium': require('./src/assets/fonts/Roboto/Roboto-Medium.ttf'),
+      Vampiro_One: require('./src/assets/fonts/Vampiro_One/VampiroOne-Regular.ttf'),
+      // 'Vampiro-One':
+      //   'https://fonts.googleapis.com/css2?family=Vampiro+One&display=swap',
+    });
+  };
+
+  if (!fontloaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => {
+          setfontloaded(true);
+        }}
+        onError={console.warn}
+      />
+    );
+  }
+
   return (
     <NavigationContainer>
       <StackNavigation />
---
+++ src/GlobalStyles/Typography.ts
@@ -0,0 +1,47 @@
+import styled from 'styled-components/native';
+
+/*
+ * font-family: 'Roboto', sans-serif;
+ * font-family: 'Vampiro One', cursive;
+ */
+
+export const H1 = styled.Text`
+  /* font-family: 'Roboto-Medium', sans-serif; */
+  font-family: sans-serif;
+  font-size: 36px;
+`;
+export const H2 = styled.Text`
+  /* font-family: 'Roboto-Medium', sans-serif; */
+  font-family: sans-serif;
+  font-size: 32px;
+`;
+export const H3 = styled.Text`
+  /* font-family: 'Roboto-Medium', sans-serif; */
+  font-family: sans-serif;
+  font-size: 28px;
+`;
+export const H4 = styled.Text`
+  /* font-family: 'Roboto-Medium', sans-serif; */
+  font-family: sans-serif;
+  font-size: 24px;
+`;
+export const H5 = styled.Text`
+  /* font-family: 'Roboto-Medium', sans-serif; */
+  font-family: sans-serif;
+  font-size: 18px;
+`;
+export const H6 = styled.Text`
+  /* font-family: 'Roboto-Medium', sans-serif; */
+  font-family: sans-serif;
+  font-size: 14px;
+`;
+export const Paragraph = styled.Text`
+  /* font-family: 'Roboto-Medium', sans-serif; */
+  font-family: sans-serif;
+  font-size: 16px;
+`;
+
+export const LogoText = styled.Text`
+  /* font-family: 'Roboto-Medium', sans-serif; */
+  font-size: 48px;
+`;
